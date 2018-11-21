af38d13c2ad84c8eb26c1ff311a3759909a54818
Fares <44028238+1-fares@users.noreply.github.com>
2018-11-21 15:08:47 +0100

    Update debian_install_software.sh

--- debian_install_software.sh
+++ debian_install_software.sh
@@ -34,6 +34,7 @@ add-apt-repository \
 	"deb [arch=amd64] https://download.docker.com/linux/debian \
 	$(lsb_release -cs) \
 	stable" && \
+apt-get update && \
 apt-get install docker-ce -y && \
 curl -L "https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose && \
 chmod +x /usr/local/bin/docker-compose && \
