02bef1b1d45616fc04d50e3c2935a8ba39c20306
Fares <44028238+1-fares@users.noreply.github.com>
2018-11-22 13:03:55 +0100

    Update debian_install_software.sh

--- debian_install_software.sh
+++ debian_install_software.sh
@@ -38,6 +38,8 @@ apt-get update && \
 apt-get install docker-ce -y && \
 curl -L "https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose && \
 chmod +x /usr/local/bin/docker-compose && \
+echo "Now type:" && \
+echo "sudo usermod -aG docker USERNAME" && \
 exit 0
 
 exit 1
