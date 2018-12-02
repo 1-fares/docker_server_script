6824337c3911c693d2974e8a67cc92efaea9a32a
Fares <44028238+1-fares@users.noreply.github.com>
2018-12-02 19:26:58 +0100

    Remove unneccessary sudo (which would fail on systems without it)

--- debian_install_software.sh
+++ debian_install_software.sh
@@ -29,7 +29,7 @@ apt-get install \
 	g++ \
 	-y \
 	&& \
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add - && \
+curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add - && \
 add-apt-repository \
 	"deb [arch=amd64] https://download.docker.com/linux/debian \
 	$(lsb_release -cs) \
