b90e0e917bb545cb47eac691bcb8c47d1522201b
Fares <44028238+1-fares@users.noreply.github.com>
2018-12-05 15:34:40 +0100

    Updated docker-compose version from 1.22.0 to 1.23.2

--- debian_install_software.sh
+++ debian_install_software.sh
@@ -36,7 +36,7 @@ add-apt-repository \
 	stable" && \
 apt-get update && \
 apt-get install docker-ce -y && \
-curl -L "https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose && \
+curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose && \
 chmod +x /usr/local/bin/docker-compose && \
 echo "Now type:" && \
 echo "sudo usermod -aG docker USERNAME" && \
